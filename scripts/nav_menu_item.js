57cdd7754798934ac02fa5e8f9878dc53914e35b
solineko <tlxm2013@gmail.com>
2019-03-13 00:45:32 +0800

    end build nav_menu

---
+++ scripts/nav_menu_item.js
@@ -0,0 +1,45 @@
+let NavMenuItemMarkObj=null;
+
+
+function SetNavMenuItemListener(){
+	let NavMenuItems;
+	NavMenuItems=GetNavMenuItems();
+	for(let i=0;i<NavMenuItems.length;i++){
+		NavMenuItems[i].addEventListener("mouseenter",HandleNavMenuItemMouseEnterEvent,false);
+		NavMenuItems[i].addEventListener("mouseleave",HandleNavMenuItemMouseLeaveEvent,false);
+		NavMenuItems[i].addEventListener("click",HandleNavMenuItemClickEvent,false);
+	}
+}
+
+function GetNavMenuItems(){
+	return document.getElementsByClassName("nav_menu_item_url");
+}
+
+function ShowNavMenuItemMark(obj){
+	obj.parentNode.style.borderWidth="2px";
+}
+
+function HideNavMenuItemMark(obj){
+	obj.parentNode.style.borderWidth="0px";
+}
+
+
+function HandleNavMenuItemMouseEnterEvent(event){
+	ShowNavMenuItemMark(event.target);
+}
+
+function HandleNavMenuItemMouseLeaveEvent(event){
+	if(event.target!=NavMenuItemMarkObj){
+		HideNavMenuItemMark(event.target);
+	}
+}
+
+function HandleNavMenuItemClickEvent(event){
+	if(event.target!=NavMenuItemMarkObj&&NavMenuItemMarkObj!=null){
+		HideNavMenuItemMark(NavMenuItemMarkObj);
+	}
+	ShowNavMenuItemMark(event.target);
+	NavMenuItemMarkObj=event.target;
+}
+
+SetNavMenuItemListener();
