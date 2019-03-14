cb4848ea369e566dde011a831836d995f4c77ee1
solineko <tlxm2013@gmail.com>
2019-03-14 22:59:01 +0800

    1.add favicon.ico null file for fix favicon not found file 2.add section_chief_carousel controller features (prev,next,autochange) 3.add jquery

---
+++ scripts/global.js
@@ -0,0 +1,10 @@
+// let result;
+// $.ajax({
+// 	url:"https://edu.telking.com/api?type=month",
+// 	type:"GET",
+// 	dataType:"jsonp",
+// 	success:function(data){
+// 		result=JSON.stringify(data);
+// 		console.log(result);
+// 	}
+// });
---
+++ scripts/section_chief_carousel.js
@@ -0,0 +1,48 @@
+let images=["https://via.placeholder.com/1280x720/f92525","https://via.placeholder.com/1280x720/4587f0","https://via.placeholder.com/1280x720/5dd054","https://via.placeholder.com/1280x720/080908","https://via.placeholder.com/1280x720/27ae1c"];
+
+let timer=setInterval(function(){ChangeNextImage()},2000);
+let CurrentPage=0;
+let CarouselObj=document.getElementById("section_chief_carousel");
+
+function ChangePrevImage(){
+	CurrentPage=CurrentPage-1;
+	CheckResetPage();
+	SetPage();
+}
+
+function ChangeNextImage(){
+	CurrentPage=CurrentPage+1;
+	CheckResetPage();
+	SetPage();
+}
+
+function StopAutoChange(){
+	clearInterval(timer);
+}
+
+function StartAutoChange(){
+	timer=setInterval(function(){ChangeNextImage()},2000);
+}
+
+function CheckResetPage(){
+	if(CurrentPage>(images.length-1)){
+		CurrentPage=0;
+	}
+	if(CurrentPage<0){
+		CurrentPage=(images.length-1);
+	}
+}
+
+function SetPage(page){
+	let temp='url('+'"'+images[CurrentPage]+'"'+")";
+	CarouselObj.style.backgroundImage=temp;
+}
+
+document.getElementById("section_chief_carousel_controller_left").addEventListener("click",ChangePrevImage,false);
+document.getElementById("section_chief_carousel_controller_right").addEventListener("click",ChangeNextImage,false);
+document.getElementById("section_chief_carousel").addEventListener("mouseleave",StartAutoChange,false);
+document.getElementById("section_chief_carousel").addEventListener("mouseenter",StopAutoChange,false);
+
+
+
+
