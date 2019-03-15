a488fb9a66e36696f9c792f1d8265ea8e8fed25f
solineko <tlxm2013@gmail.com>
2019-03-15 16:48:34 +0800

    1.end build section_demonstration_curve

---
+++ scripts/section_demonstration_curve.js
@@ -0,0 +1,47 @@
+let curve_echart=echarts.init(document.getElementById('section_demonstration_curve'));
+
+
+
+$.ajax({
+	url:"https://edu.telking.com/api?type=month",
+	type:"GET",
+	dataType:"jsonp",
+	success:function(data){
+		// JSON.stringify(data);
+		// console.log(data["data"]);
+		curve_echart.setOption({
+			title:{
+				text:"曲线数据展示",
+			},
+			xAxis:{
+				name:"日期",
+				data:data["data"]["xAxis"],
+				axisLine:{
+					show:false,
+				},
+			},
+			yAxis:{
+				name:"人",
+				axisLine:{
+					show:false,
+				},
+			},
+			series:{
+				type:"line",
+				data:data["data"]["series"],
+				label:{
+					show:true,
+				},
+				smooth:true,
+				itemStyle:{
+					normal:{
+						color:"#4586ef",
+					},
+				},
+			},
+			lineStyle:{
+				color:"#94b7f5",
+			},
+		});
+	},
+});
